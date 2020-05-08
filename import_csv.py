6e9aee592e0472e68895c792afd529483830550c
Daniel Welsh <dan.welsh@live.com.au>
2020-05-09 09:43:00 +1000

    initial commit

--- import_csv.py
+++ import_csv.py
@@ -1,20 +1,5 @@
-import math
-import os
-import sys
-
 import requests
 
-print(sys.version)
-print(sys.executable)
-
-
-def greet(name):
-    greeting = "hello, {}".format(name)
-    return greeting
-
-
-# inputname = input("Your Name? ")
-print(greet("Dan"))
-
-# r = requests.get('http://openedsauce.com')
-# print(r.status_code)
+r = requests.get('http://openedsauce.com')
+print(r.status_code)
+print(r.ok)
